d02e3444fa814890e2d2f3b633491e698c633ac8
“mohanad <v-mohanadhaj@microsoft.com>
2022-12-15 13:58:12 +0200

    adding selector

--- selectors/repoPage.selectors.ts
+++ selectors/repoPage.selectors.ts
@@ -1,16 +1,23 @@
 export const repoPageSelectors =
 {
-    codeTab: '#code-tab',
-    settingsTab: '#settings-tab',
+    // create new repo selectors
     newRepoNameSelector: '#repository_name',
     validiationSelector: '#input-check-215',
     repository_description: '#repository_description',
     repository_visibility_public: '#repository_visibility_public',
     repository_visibility_private: '#repository_visibility_private',
+
+    // under delete tab
+    settingsTab: '#settings-tab',
     createRepoButton: '[data-disable-with="Creating repository&hellip;"]',
     renameField: '#rename-field',
     deleteConfirmationLabel: '[aria-label="Type in the name of the repository to confirm that you want to delete this repository."]',
     projectTab: '#projects-tab',
     linkOrAddProjectButton: '#link-or-add-project-button',
-    searhForProjectButton: '#query'
+    searhForProjectButton: '#query',
+
+    // code button area to clone repo
+    codeTab: '#code-tab',
+    codeButtonForLink: 'summary.btn-primary',
+    localTabButton: '#local-tab'
 }
